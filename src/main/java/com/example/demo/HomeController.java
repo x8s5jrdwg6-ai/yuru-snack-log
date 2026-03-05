ecd64938003a67b209ae1e793cb2ae87e2479afb
nolot <x8s5jrdwg6-ai@users.noreply.github.comecho>
2026-03-05 23:22:07 +0900

    DB対応

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -21,12 +21,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 
 @Controller
 public class HomeController {
+	@GetMapping("/healthz")
+	@ResponseBody
+	public String healthz() {
+	    return "ok";
+	}
 	/*--------------------------------------
 		定数
 	--------------------------------------*/
