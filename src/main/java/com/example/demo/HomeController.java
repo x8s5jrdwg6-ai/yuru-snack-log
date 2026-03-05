cbb4fa01b8fbfb1d19a957881847b602c880a9d1
nolot <x8s5jrdwg6-ai@users.noreply.github.comecho>
2026-03-05 23:31:29 +0900

    DB 対応

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -28,6 +28,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class HomeController {
+	
 	@GetMapping("/healthz")
 	@ResponseBody
 	public String healthz() {
